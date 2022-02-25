f93dbc5673ed29709216a3a9649f4919f9e093f1
GustavoPD <gudalmoro@gmail.com>
2022-02-25 06:45:53 -0300

    Corrige 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,10 @@
 SELECT 
-	ROUND(MIN(S.plan_value), 2) AS faturamento_minimo,
-	ROUND(MAX(S.plan_value), 2) AS faturamento_maximo,
+	MIN(S.plan_value) AS faturamento_minimo,
+	MAX(S.plan_value) AS faturamento_maximo,
 	ROUND(AVG(S.plan_value), 2) AS faturamento_medio,
 	ROUND(SUM(S.plan_value), 2) AS faturamento_total
-FROM SpotifyClone.subscriptions AS S
-	JOIN SpotifyClone.users AS U
-ON S.plan_id = U.plan_id;
+FROM 
+	SpotifyClone.subscriptions AS S
+JOIN 
+	SpotifyClone.users AS U
+    ON S.plan_id = U.plan_id;
