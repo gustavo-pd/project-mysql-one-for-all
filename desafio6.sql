69a4f107271088d97bd1e531ca1673bfcd26cb76
GustavoPD <gudalmoro@gmail.com>
2022-02-25 06:28:42 -0300

    Corrige desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,6 +1,6 @@
 SELECT 
-	ROUND(MIN(plan_value), 2) AS faturamento_minimo,
-	ROUND(MAX(plan_value), 2) AS faturamento_maximo,
+	ROUND(MIN(S.plan_value), 2) AS faturamento_minimo,
+	ROUND(MAX(S.plan_value), 2) AS faturamento_maximo,
 	ROUND(AVG(S.plan_value), 2) AS faturamento_medio,
 	ROUND(SUM(S.plan_value), 2) AS faturamento_total
 FROM SpotifyClone.subscriptions AS S
