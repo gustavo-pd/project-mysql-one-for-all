7ac2ce0c7fbb868ea96daf87a53949af40ec8eb0
GustavoPD <gudalmoro@gmail.com>
2022-02-25 08:20:46 -0300

    Req 10 n ta passando

--- desafio10.sql
+++ desafio10.sql
@@ -1,13 +1,9 @@
-SELECT
-  T.name AS nome,
-  COUNT(H.user_id) AS reproducoes
-FROM SpotifyClone.user_historic AS H
-JOIN SpotifyClone.tracks AS T
-  ON H.track_id = T.track_id
-JOIN SpotifyClone.users AS U
-  ON U.user_id = H.user_id
-JOIN SpotifyClone.subscriptions AS S
-  ON S.plan_id = U.plan_id
-WHERE S.plan_name = 'gratuito' OR S.plan_name = 'pessoal'
-GROUP BY nome
-ORDER BY nome;
+SELECT tracks.name AS nome,
+	COUNT(*) AS reproducoes
+FROM user_historic AS H
+JOIN tracks AS T
+	ON H.track_id = T.track_id
+JOIN users AS U
+	ON U.plan_id = 1 OR U.plan_id = 4
+GROUP BY T.name
+ORDER BY T.name;
